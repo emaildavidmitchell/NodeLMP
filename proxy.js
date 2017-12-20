709ef05acf6a7e47e64bd04f1ad689e361c78791
Marcel <marcelb@uvic.ca>
2017-12-19 16:05:14 -0800

    Add additional logging

--- proxy.js
+++ proxy.js
@@ -9,32 +9,38 @@ var app_server = "http://localhost:3000";
 var topic_modeller_server = "http://localhost:3001";
 
 app.use("/", function(request, response) {
-    console.log('redirecting to main app');
+    console.log('redirecting to /');
+    console.log(request)
     proxy.web(request, response, {target: app_server});
 });
 
 app.use("/examples", function(request, response) {
-    console.log('redirecting to main app');
+    console.log('redirecting to /examples');
+    console.log(request)
     proxy.web(request, response, {target: app_server});
 });
 
 app.use("/network", function(request, response) {
-    console.log('redirecting to main app');
+    console.log('redirecting to /network');
+    console.log(request)
     proxy.web(request, response, {target: app_server});
 });
 
 app.use("/about", function(request, response) {
-    console.log('redirecting to main app');
+    console.log('redirecting to /about');
+    console.log(request)
     proxy.web(request, response, {target: app_server});
 });
 
 app.use("/contribute", function(request, response) {
-    console.log('redirecting to main app');
+    console.log('redirecting to /contribute');
+    console.log(request)
     proxy.web(request, response, {target: app_server});
 });
 
 app.use("/topic_modeller", function(request, response) {
-    console.log('redirecting to topic modeller');
+    console.log('redirecting to /topic_modeller');
+    console.log(request)
     proxy.web(request, response, {target: topic_modeller_server});
 });
 
